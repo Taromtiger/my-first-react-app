b46dc9d5ba5662b474fab5b5ff99aae587a26cf5
Tarom Tiger <you@example.com>
2022-04-25 16:23:47 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,79 +1,25 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/counter";
+import React from "react";
+import { BrowserRouter, Route, } from "react-router-dom";
 import "./styles/App.css"
-import PostItem from "./components/PostItem";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import { usePosts } from "./hooks/usePosts";
-import axios from "axios";
-import PostService from "./API/PostService";
+import About from "./pages/About"
+import Posts from "./pages/Posts"
 
 function App() {
 
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({ sort: '', query: '' })
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostLoading, setIsPostLoading] = useState(false);
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
-
-
-  async function fetchPosts() {
-    setIsPostLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts)
-      setIsPostLoading(false);
-    }, 1000)
-  }
-
-
-  // Отримуємо post із дочірнього компонента
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
-
   return (
-    <div className="App">
 
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Створити пост
-      </MyButton>
+    <BrowserRouter>
 
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
+      <Route path="/about">
+        <About />
+      </Route>
 
-      <hr style={{ margin: '15px 0' }} />
+      <Route path="/posts">
+        <Posts />
+      </Route>
 
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter}
-      />
-
-      {isPostLoading
-        ? <h1 style={{ textAlign: 'center' }}>Відбувається завантаження...</h1>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Пости про JS" />
-      }
-
-    </div >
-  );
+    </BrowserRouter>
+  )
 }
 
 export default App;
